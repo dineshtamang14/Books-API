e1e542d32783b10a5502fed125d5cdec4aada274
Dinesh Tamang <dineshtamang7263@gmail.com>
2022-03-03 12:27:58 +0530

    add stipe route

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const authRoute = require("./routes/auth");
 const productRoute = require("./routes/product");
 const cartRoute = require("./routes/cart");
 const orderRoute = require("./routes/order");
+const paymentRoute = require("./routes/stripe");
 dotenv.config();
 
 
@@ -29,6 +30,7 @@ app.use("/users", userRoute);
 app.use("/products", productRoute);
 app.use("/carts", cartRoute);
 app.use("/orders", orderRoute);
+app.use("/payment", paymentRoute);
 app.use((req, res)=> {
     res.status(404).json({
         msg: "404 NOT FOUND!"
