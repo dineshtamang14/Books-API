9dc40511bde792dce8ba82563df693b8b452a3b4
Dinesh Tamang <dineshtamang7263@gmail.com>
2022-03-06 19:24:17 +0530

    update index.js

--- index.js
+++ index.js
@@ -14,10 +14,10 @@ dotenv.config();
 
 const app = express();
 
+app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors());
 
 const port = process.env.PORT || 5000;
 const mongodburl = process.env.URL;
--- routes/stripe.js
+++ routes/stripe.js
@@ -7,7 +7,7 @@ router.post("/payment", (req, res) => {
     stripe.charges.create({
         source: req.body.tokenId,
         amount: req.body.amount,
-        currency: "usd",
+        currency: "inr",
     }, (stripeErr, stripeRes) => {
         if(stripeErr){
             res.status(500).json(stripeErr);
